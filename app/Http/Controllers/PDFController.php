ad17fc4637cd30838def0ea436ca4ca860ce6303
ngan_ct <caothuongngan@gmail.com>
2023-11-06 11:58:18 +0700

    commit

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -93,6 +93,7 @@ class PDFController extends Controller
                 throw $exception;
             }
         }
+        dd($pathPDF);
         if (file_exists($pathPDF)) {
             $headers = ['Content-Type' => 'application/pdf'];
             $response = Response::download($pathPDF, $namePDF, $headers);
